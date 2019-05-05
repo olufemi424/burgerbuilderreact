43af5a08043155d17f609f119bd4f97ce931a9e3
Olufemi Afolabi <femiafolabidiipo@gmail.com>
2019-05-05 17:40:09 -0500

    add ingredient method

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -9,10 +9,14 @@ const controls = [
   { label: "Meat", type: "meat" }
 ];
 
-const BuildControls = () => (
+const BuildControls = props => (
   <div className={classes.BuildControls}>
     {controls.map(ctrl => (
-      <BuildControl key={ctrl.label} label={ctrl.label} />
+      <BuildControl
+        key={ctrl.label}
+        label={ctrl.label}
+        added={() => props.ingredientAdded(ctrl.type)}
+      />
     ))}
   </div>
 );

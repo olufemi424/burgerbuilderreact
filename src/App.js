c05ba935ed4db4d3971ba3fc111cf26fbaf5a9bf
Olufemi Afolabi <femiafolabidiipo@gmail.com>
2019-05-18 00:51:47 -0500

    checkout page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 
 import Layout from "./hoc/Layout/Layout";
 import BurderBulder from "./containers/BurgerBuilder/BurgerBuilder";
+import Checkout from "./containers/Checkout/Checkout";
 
 function App() {
   return (
     <div>
       <Layout>
         <BurderBulder />
+        <Checkout />
       </Layout>
     </div>
   );
